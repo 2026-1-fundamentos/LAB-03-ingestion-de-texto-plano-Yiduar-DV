63296584d11eab276b2a38140c647756e2073298
Yiduaaar <yidocar@gmail.com>
2026-07-02 17:51:22 -0500

    6

---
+++ homework/pregunta_06.py
@@ -0,0 +1,22 @@
+"""
+Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
+datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
+`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
+librerias de pandas para resolver las preguntas.
+"""
+
+import pandas as pd
+
+def pregunta_06():
+    """
+    Retorne una lista con los valores unicos de la columna `c4` del archivo
+    `tbl1.csv` en mayusculas y ordenados alfabéticamente.
+
+    Rta/
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    """
+    tbl1 = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+    return sorted(tbl1["c4"].str.upper().unique().tolist())
+
+print(pregunta_06())
