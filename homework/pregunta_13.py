69eb01d74ebca855348801757bbf98d6b4f7baa8
Yiduaaar <yidocar@gmail.com>
2026-07-02 17:51:47 -0500

    solucion

---
+++ homework/pregunta_13.py
@@ -0,0 +1,29 @@
+"""
+Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
+datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
+`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
+librerias de pandas para resolver las preguntas.
+"""
+
+import pandas as pd
+
+def pregunta_13():
+    """
+    Si la columna `c0` es la clave en los archivos `tbl0.tsv` y `tbl2.tsv`,
+    compute la suma de `tbl2.c5b` por cada valor en `tbl0.c1`.
+
+    Rta/
+    c1
+    A    146
+    B    134
+    C     81
+    D    112
+    E    275
+    Name: c5b, dtype: int64
+    """
+    tbl0 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    tbl2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    merged = tbl0.merge(tbl2, on="c0")
+    return merged.groupby("c1")["c5b"].sum()
+
+print(pregunta_13())
